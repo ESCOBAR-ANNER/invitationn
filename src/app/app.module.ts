2280f59e373b27e7a7e891dae004de364a690d69
Anner Escobar <aescobarc13@miumg.edu.gt>
2024-03-21 17:30:56 -0600

    Actualizando previo a actualizar angualr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { TemporizadorComponent } from './temporizador/temporizador.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemporizadorComponent
+    TemporizadorComponent,
+    GaleriaComponent,
+    
   ],
   imports: [
     BrowserModule,
     RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
